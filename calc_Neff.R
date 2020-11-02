136c6b69282c99bea6d5f65bcb25937882093a28
sarahcolbert <sarah@Sarahs-MacBook-Pro.local>
2020-11-02 08:48:06 -0700

    added MAF restriction to Neff calc

--- calc_Neff.R
+++ calc_Neff.R
@@ -1,6 +1,9 @@
 ### Read in summary statistics for latent factor which you wish to calculate Neff for
 factor <- read.table("FNUMBER_sumstats.txt", header = TRUE)
 
+### Restrict to MAF of 40% and 10%
+factor<-subset(factor, factor$MAF <= .4 & factor$MAF >= .1)
+
 ### Calculate Neff using equation from Mallard et al.
 Effective_N<-(mean(((factor$Z_Estimate/factor$est)^2)/(2*factor$MAF*(1-factor$MAF))))
 
