20db156e9f1951a2293963769c986c9a8a0a61f9
Sarah Colbert <sarah.colbert@wustl.edu>
2021-09-03 14:47:27 -0400

    fixed so array job starts at 0

--- multi_GWAS.bash
+++ multi_GWAS.bash
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=8
 #SBATCH --output=./outerr/set.%a.out
 #SBATCH --error=./outerr/set.%a.err
-#SBATCH --array=1-#OF SNP SUBSETS
+#SBATCH --array=0-#OF SNP SUBSETS-1
 
 ### display time at beginning and end of script to track how long it takes
 date
