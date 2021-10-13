aac0f8c94e82c59056f9422b3b676e41627a9558
Sarah Colbert <47012384+sarahcolbert@users.noreply.github.com>
2021-10-13 10:38:30 -0500

    added code to produce progress updates in log file

--- multi_GWAS.R
+++ multi_GWAS.R
@@ -4,13 +4,19 @@ require(GenomicSEM)
 library(dplyr)
 
 ### load the summary statistics RData file in the split form
+print("loading summary statistics from set NUMBER...")
 split_sumstats <- read.table("./split_sumstats/sumstatsNUMBER.txt", header = TRUE)
+print("finished loading summary statistics from set NUMBER")
 
 ### load the LDSC covariance matrix
+print("loading LDSC covariance matrix...")
 load("LDSCoutput.RData")
+print("finished loading LDSC covariance matrix")
 
 ### run a common factor model for one subset of SNPs
+print("running models...")
 CommonFactor <- commonfactorGWAS(covstruc=LDSCoutput, SNPs=split_sumstats)
+print("GWAS completed")
 
 ### remove unnecessary columns from the dataframe
 CommonFactor$free <- NULL
@@ -20,6 +26,11 @@ CommonFactor$op <- NULL
 CommonFactor$rhs <- NULL
 
 ### remove any rows that produce a warning
+print("removing SNPs that produce a warning...")
 CF <- filter(CommonFactor, warning==0)
+print("SNPs with warnings removed")
 
+print("writing results to file...")
 write.table(CF, file="./results/NUMBER.txt", row.names=FALSE, quote=FALSE)
+print("analysis for set NUMBER complete")
+
